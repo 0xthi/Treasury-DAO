56a7a3df1d50b784c936e11cfa3b5ed94ac932a8
Thileepan-Ilankumaran <112769282+Thileepan-Ilankumaran@users.noreply.github.com>
2024-11-04 11:24:52 +0530

    resolve added in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      '@uniswap/permit2-sdk': require.resolve('@uniswap/permit2-sdk'),
     },
   },
   build: {
